44071aa0c563e34800e5378ea8df6e8607e8a8f6
Martin R <martin_riveiro@hotmail.com>
2024-06-05 17:27:39 -0300

    bug-fix: actualizacion dockerfile y  parametros de query en app.py

--- API/app/app.py
+++ API/app/app.py
@@ -1,19 +1,39 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy import text
 from sqlalchemy.exc import SQLAlchemyError
 
 app = Flask(__name__)
+engine = create_engine('mysql+mysqlconnector://app_user:appMate123@db/flaskdb')
 
-def set_connection():
+# def set_connection():
+#     try:
+#         engine = create_engine('mysql+mysqlconnector://app_user:appMate123@db/flaskdb')
+#         connection = engine.connect()
+#         return connection
+#     except SQLAlchemyError as e:
+#         error = str(e.__cause__)
+#         return error
+    
+@app.route('/users/<id>', methods=['DELETE'])
+def delete_user(id):
+    query = text("DELETE FROM Users WHERE id = :id")
+    validation_query = text("SELECT * FROM Users WHERE id = :id")
     try:
-        engine = create_engine('mysql+mysqlconnector://app_user:appMate123@db/flaskdb')
-        connection = engine.connect()
-        return connection
+        conn = engine.connect()
+        val_result = conn.execute(validation_query, {'id': id})
+        if val_result.rowcount == 0:
+            conn.close()
+            return jsonify({"message": "Usuario no encontrado"}), 404
+        conn.execute(query, {'id': id})
+        conn.commit()
+        conn.close()
+        return jsonify({"message": "Usuario eliminado correctamente"}), 200
     except SQLAlchemyError as e:
         error = str(e.__cause__)
-        return error
+        return jsonify({"message": error}) , 500
+    
 
 @app.route('/')
 def home():
