5d60e96cea12293b2baa69722b9d06e855682d43
Martin R <martin_riveiro@hotmail.com>
2024-06-06 00:48:45 -0300

    bug-fix: errores en copia de proyecto de api para frontend

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,39 +1,6 @@
 from flask import Flask, request, url_for, jsonify, render_template, redirect
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine
-from sqlalchemy import text
-from sqlalchemy.exc import SQLAlchemyError
 
 app = Flask(__name__)
-engine = create_engine('mysql+mysqlconnector://app_user:appMate123@db/flaskdb')
-
-# def set_connection():
-#     try:
-#         engine = create_engine('mysql+mysqlconnector://app_user:appMate123@db/flaskdb')
-#         connection = engine.connect()
-#         return connection
-#     except SQLAlchemyError as e:
-#         error = str(e.__cause__)
-#         return error
-    
-@app.route('/users/<id>', methods=['DELETE'])
-def delete_user(id):
-    query = text("DELETE FROM Users WHERE id = :id")
-    validation_query = text("SELECT * FROM Users WHERE id = :id")
-    try:
-        conn = engine.connect()
-        val_result = conn.execute(validation_query, {'id': id})
-        if val_result.rowcount == 0:
-            conn.close()
-            return jsonify({"message": "Usuario no encontrado"}), 404
-        conn.execute(query, {'id': id})
-        conn.commit()
-        conn.close()
-        return jsonify({"message": "Usuario eliminado correctamente"}), 200
-    except SQLAlchemyError as e:
-        error = str(e.__cause__)
-        return jsonify({"message": error}) , 500
-    
 
 @app.route('/')
 def home():
--- Frontend/init.sh
+++
@@ -1,5 +0,0 @@
-# pip3 install flask
-
-# pip3 install flask_sqlalchemy
-
-# pip3 install mysql-connector-python
