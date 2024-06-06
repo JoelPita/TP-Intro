44ebfc91a00bb5d4c582df63f77e4b7bc4869019
Martin R <martin_riveiro@hotmail.com>
2024-06-06 18:51:34 -0300

    #30 feat: endpoints para gestionar reservas (get, alta y confirmacion)

--- API/app/app.py
+++ API/app/app.py
@@ -6,16 +6,120 @@ from sqlalchemy.exc import SQLAlchemyError
 
 app = Flask(__name__)
 engine = create_engine('mysql+mysqlconnector://app_user:appMate123@db/flaskdb')
-
-# def set_connection():
-#     try:
-#         engine = create_engine('mysql+mysqlconnector://app_user:appMate123@db/flaskdb')
-#         connection = engine.connect()
-#         return connection
-#     except SQLAlchemyError as e:
-#         error = str(e.__cause__)
-#         return error
     
+    
+@app.route('/reservas', methods=['POST'])
+def create_reserva():
+    data = request.json
+    query = text("""
+        INSERT INTO Reservas (email_cliente, nombre_cliente, telefono_cliente, fecha_desde, fecha_hasta, 
+            cantidad_habitaciones, cantidad_personas, metodo_pago, estado, precio_total, habitacion_id) 
+        VALUES (:email_cliente, :nombre_cliente, :telefono_cliente, :fecha_desde, :fecha_hasta, :cantidad_habitaciones, 
+            :cantidad_personas, :metodo_pago, :estado, :precio_total, :habitacion_id)
+        """)
+    
+    try:
+        conn = engine.connect()
+        conn.execute(query, {
+            'email_cliente': data['email_cliente'],
+            'nombre_cliente': data['nombre_cliente'],
+            'telefono_cliente': data['telefono_cliente'],
+            'fecha_desde': data['fecha_desde'],
+            'fecha_hasta': data['fecha_hasta'],
+            'cantidad_habitaciones': data['cantidad_habitaciones'],
+            'cantidad_personas': data['cantidad_personas'],
+            'metodo_pago': data['metodo_pago'],
+            'estado': "pendiente",
+            'precio_total': data['precio_total'],
+            'habitacion_id': data['habitacion_id']
+        })
+        conn.commit()
+        conn.close()
+        return jsonify({"success": True, "message": "Reserva añadida correctamente"}), 200
+    except SQLAlchemyError as e:
+        error = str(e.__cause__)
+        conn.close()
+        return jsonify({"message": error}), 500
+    
+@app.route('/reservas', methods=['GET'])
+def get_reservas():
+    estado = request.args.get('estado')
+    if estado:
+        query = text("SELECT * FROM Reservas WHERE estado = :estado")
+        params = {'estado': estado}
+    else:
+        query = text("SELECT * FROM Reservas")
+        params = {}
+
+    try:
+        conn = engine.connect()
+        result = conn.execute(query, params)
+        reservas = []
+        for row in result:
+            reserva = {
+                'id': row[0],
+                'email_cliente': row[1],
+                'nombre_cliente': row[2],
+                'telefono_cliente': row[3],
+                'fecha_desde': row[4],
+                'fecha_hasta': row[5],
+                'cantidad_habitaciones': row[6],
+                'cantidad_personas': row[7],
+                'metodo_pago': row[8],
+                'estado': row[9],
+                'motivo_rechazo': row[10],
+                'precio_total': row[11],
+                'habitacion_id': row[12]
+            }
+            reservas.append(reserva)
+        conn.close()
+        return jsonify({"success": True, "reservas": reservas}), 200
+    except SQLAlchemyError as e:
+        error = str(e.__cause__)
+        conn.close()
+        return jsonify({"success": False, "message": error}), 500
+    
+
+
+@app.route('/reservas/<int:id>/confirm', methods=['PUT'])
+def confirm_reserva(id):
+    data = request.json
+    estado = data['estado']
+    
+    if estado not in ['aceptada', 'rechazada']:
+        return jsonify({"success": False, "message": "Estado inválido. Solo se permite 'aceptada' o 'rechazada'."}), 400
+    
+    motivo_rechazo = data['motivo_rechazo']
+    query = text("""
+        UPDATE Reservas 
+        SET estado = :estado, motivo_rechazo = :motivo_rechazo
+        WHERE id = :id
+        """)
+        
+    try:
+        conn = engine.connect()
+        conn.execute(query, {
+            'estado': estado,
+            'motivo_rechazo': motivo_rechazo,
+            'id': id
+        })
+        conn.commit()
+        conn.close()
+        
+        #TO DO: IMPLEMENTAR ENVIO DE MAIL
+        send_email(data['email_cliente'], estado, motivo_rechazo)
+
+        return jsonify({"success": True, "message": "Reserva actualizada correctamente"}), 200
+    except SQLAlchemyError as e:
+        error = str(e.__cause__)
+        conn.close()
+        return jsonify({"success": False, "message": error}), 500
+
+def send_email(email, estado, motivo_rechazo):
+    pass
+
+
+
 @app.route('/users/<id>', methods=['DELETE'])
 def delete_user(id):
     query = text("DELETE FROM Users WHERE id = :id")
@@ -33,7 +137,6 @@ def delete_user(id):
     except SQLAlchemyError as e:
         error = str(e.__cause__)
         return jsonify({"message": error}) , 500
-    
 
 @app.route('/')
 def home():
