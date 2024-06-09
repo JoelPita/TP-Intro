94d95eb133fc0aad681a5776db80b8358e7606f2
Cristian <cvaldez@fi.uba.ar>
2024-06-09 14:00:26 -0300

    Frotend-PaginaPrincipal: Creacion de la seccion about

--- API/app/app.py
+++ API/app/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, render_template, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from .reviews.routes import reviews_bp
 from .reservas.routes import reservas_bp
@@ -19,7 +20,40 @@ app.register_blueprint(users_bp, url_prefix='/users')
 #Esta podria estar en un archivo route
 @app.route('/')
 def home():
-    return jsonify({"message": "Hello, Docker!"})
+    return render_template('index.html')
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+@app.route('/habitaciones')
+def habitaciones():
+    return render_template('habitaciones.html')
+
+@app.route('/habitaciones/<id>', methods = ['GET'])
+def habitaciones_id(id):
+    return
+
+
+@app.route('/servicios')
+def servicios():
+    return render_template('servicios.html')
+
+@app.route('/contacto')
+def contacto():
+    return render_template('contacto.html')
+
+@app.route('/reserva')
+def reserva():
+    return render_template('reserva.html')
+
+@app.route('/reviews')
+def reviews():
+    return render_template('reviews.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
 
 
 if __name__ == "__main__":
