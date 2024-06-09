f3d014ee42f956368da1f6b01d0c61c6cea58bed
Cristian <cvaldez@fi.uba.ar>
2024-06-09 14:00:26 -0300

    Frotend-PaginaPrincipal: Creacion de la seccion about

--- API/app/app.py
+++ API/app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy import text
@@ -37,7 +37,41 @@ def delete_user(id):
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Hello, Docker!"})
+    return render_template('index.html')
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+@app.route('/habitaciones')
+def habitaciones():
+    return render_template('habitaciones.html')
+
+@app.route('/habitaciones/<id>', methods = ['GET'])
+def habitaciones_id(id):
+    return
+
+
+@app.route('/servicios')
+def servicios():
+    return render_template('servicios.html')
+
+@app.route('/contacto')
+def contacto():
+    return render_template('contacto.html')
+
+@app.route('/reserva')
+def reserva():
+    return render_template('reserva.html')
+
+@app.route('/reviews')
+def reviews():
+    return render_template('reviews.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
 
 if __name__ == "__main__":
     app.run(host='127.0.0.0.0', port=5000, debug=True) 
--- Frontend/app.py
+++ Frontend/app.py
@@ -1,10 +1,48 @@
-from flask import Flask, request, url_for, jsonify, render_template, redirect
+from flask import Flask, jsonify, request, render_template, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy import text
+from sqlalchemy.exc import SQLAlchemyError
 
 app = Flask(__name__)
-
+engine = create_engine('mysql+mysqlconnector://app_user:appMate123@db/flaskdb')
+    
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+@app.route('/habitaciones')
+def habitaciones():
+    return render_template('habitaciones.html')
+
+@app.route('/habitaciones/<id>', methods = ['GET'])
+def habitaciones_id(id):
+    return
+
+@app.route('/servicios')
+def servicios():
+    return render_template('servicios.html')
+
+@app.route('/contacto')
+def contacto():
+    return render_template('contacto.html')
+
+@app.route('/reserva')
+def reserva():
+    return render_template('reserva.html')
+
+@app.route('/reviews')
+def reviews():
+    return render_template('reviews.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
 
 if __name__ == "__main__":
     app.run(host='127.0.0.0.0', port=5000, debug=True) 
