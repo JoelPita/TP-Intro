0c9a50b3d2ec54451317e1e9364e400a2b8d10a3
MatiasWoi <mwoiciechowski@fi.uba.ar>
2024-06-11 04:11:36 -0300

    Vuelvo a subir mi modulo

--- API/app/app.py
+++ API/app/app.py
@@ -4,6 +4,7 @@ from sqlalchemy import create_engine
 from .reviews.routes import reviews_bp
 from .reservas.routes import reservas_bp
 from .users.routes import users_bp
+from .gestion_precios.routes import gestion_precios_bp
 import logging
 
 app = Flask(__name__)
@@ -19,6 +20,8 @@ except Exception as e:
 app.register_blueprint(reviews_bp, url_prefix='/reviews')
 app.register_blueprint(reservas_bp, url_prefix='/reservas')
 app.register_blueprint(users_bp, url_prefix='/users')
+app.register_blueprint(gestion_precios_bp, url_prefix='/gestion_precios')
+
 
 if __name__ == "__main__":
     app.run(host='127.0.0.0', port=5000, debug=True) 
