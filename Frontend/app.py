bf92b4586292cac1f75271ce9f22ccfaca8e2abb
Martin R <martin_riveiro@hotmail.com>
2024-06-13 19:34:23 -0300

    feat-refactor: clean del codigo base, form basico de reservas y gestion

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,10 +1,4 @@
 from flask import Flask, jsonify, request, render_template, url_for, redirect
-import smtplib
-from email.mime.text import MIMEText
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine
-from sqlalchemy import text
-from sqlalchemy.exc import SQLAlchemyError
 import requests
 import datetime
 """
@@ -16,7 +10,6 @@ LONGITUD = "-72.25593247083323"
 UNIDAD = "metric"
 
 app = Flask(__name__)
-engine = create_engine('mysql+mysqlconnector://app_user:appMate123@db/flaskdb')
 
 """
     Método que se encarga de la comunicación con el servicio de Open Weather y se obtiene
@@ -120,9 +113,45 @@ def contacto():
         return render_template('contacto.html')
     return render_template('contacto.html')
 
-@app.route('/reserva')
+@app.route('/reserva', methods=["GET", "POST"])
 def reserva():
+    if request.method == "POST":
+        data = {
+            'email_cliente': request.form['email_cliente'],
+            'nombre_cliente': request.form['nombre_cliente'],
+            'telefono_cliente': request.form['telefono_cliente'],
+            'fecha_desde': request.form['fecha_desde'],
+            'fecha_hasta': request.form['fecha_hasta'],
+            'cantidad_habitaciones': request.form['cantidad_habitaciones'],
+            'cantidad_personas': request.form['cantidad_personas'],
+            'metodo_pago': request.form['metodo_pago'],
+            'estado': "Pendiente",  # Estado predeterminado
+            'habitacion_id': request.form['habitacion_id'],
+            'precio_total': 0  # to do: calcular precio total
+        }
+        print("Datos recibidos del formulario:", data)
+        api_url = "http://127.0.0.1:5000/reservas"
+        headers = {'Content-Type': 'application/json'}
+        
+        response = requests.post(api_url, json=data, headers=headers)
+        print("Respuesta de la API:", response.status_code, response.text)
+        
+        if response.status_code == 200:
+            return render_template('confirmacion_reserva.html')
+        else:
+            return jsonify({"message": "Error al crear la reserva"}), 500
+    
     return render_template('reserva.html')
+
+@app.route('/gestion_reservas')
+def gestion_reservas():
+    api_url = "http://127.0.0.1:5000/reservas"
+    response = requests.get(api_url)
+    if response.status_code == 200:
+        reservas = response.json().get("reservas", [])
+        return render_template('gestion_reservas.html', reservas=reservas)
+    else:
+        return jsonify({"message": "Error al obtener las reservas"}), 500
 
 @app.route('/reviews')
 def reviews():
@@ -134,4 +163,4 @@ def page_not_found(e):
 
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.0.0', port=5000, debug=True) 
+    app.run(host='127.0.0.1', port=5001, debug=True)
