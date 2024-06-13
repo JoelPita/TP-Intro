6bd55811cff0d648f5e4e445618c85256646820e
CarlaMendoza90 <mendozacoronadocarla@gmail.com>
2024-06-13 17:55:50 -0300

    Add: se agrega codigo para enviar mails.

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify, request, render_template, url_for, redirect
+import smtplib
+from email.mime.text import MIMEText
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy import text
@@ -98,7 +100,24 @@ def servicios():
 @app.route('/contacto', methods=["GET", "POST"])
 def contacto():
     if request.method == "POST":
-        return "Es tremendo que funcione"
+        nombre = request.form.get("name")
+        email = request.form.get("email")
+        asunto = request.form.get("subject")
+        mensaje_contenido = request.form.get("message")
+
+        mensaje_completo = (f"NOMBRE: {nombre}\n"
+                            f"MAIL: {email}\n"
+                            f"MENSAJE:\n{mensaje_contenido}")
+
+        mensaje = MIMEText(mensaje_completo)
+        mensaje["Subject"] = asunto
+        mensaje["From"] = "hotel.glaciar.argentina@gmail.com"
+        mensaje["To"] = "hotel.glaciar.argentina@gmail.com"
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
+            smtp_server.login("hotel.glaciar.argentina@gmail.com", "oqvu xdib bmnp ymzy")
+            smtp_server.sendmail("hotel.glaciar.argentina@gmail.com", "hotel.glaciar.argentina@gmail.com", mensaje.as_string())
+
+        return render_template('contacto.html')
     return render_template('contacto.html')
 
 @app.route('/reserva')
