0fe74dfe98ed6175ccb735a7e67e17aed3cc74fc
MatiasWoi <mwoiciechowski@fi.uba.ar>
2024-06-18 10:45:04 -0300

    Modularizo con blueprint y cambio colores de la vista gestion_precios

--- Frontend/app.py
+++ Frontend/app.py
@@ -5,6 +5,7 @@ from blueprints.weatherAPI.weather import weatherBp
 from blueprints.contacto.contacto import contactoBp
 from blueprints.reserva.reserva import reservaBp
 from blueprints.gestion_reservas.gestion_reserva import gestionReservaBp
+from blueprints.gestion_precios.gestion_precios import gestionPreciosBp
 from blueprints.admin.reviews import admin_reviewsBp
 from blueprints.reviews.reviews import reviewsBp
 
@@ -13,6 +14,7 @@ app.register_blueprint(weatherBp)
 app.register_blueprint(contactoBp, url_prefix="/contacto")
 app.register_blueprint(reservaBp, url_prefix="/reserva")
 app.register_blueprint(gestionReservaBp, url_prefix="/gestion_reservas")
+app.register_blueprint(gestionPreciosBp, url_prefix="/gestion_precios")
 app.register_blueprint(admin_reviewsBp, url_prefix="/admin/reviews")
 app.register_blueprint(reviewsBp, url_prefix="/reviews")
 
@@ -41,75 +43,5 @@ def page_not_found(e):
     return render_template('404.html'),404
 
 
-@app.route('/actualizar_precios', methods = ['GET'])
-def pedir_precios_habitaciones():
-    obtener_habitaciones = 'http://127.0.0.1:5000/gestion_precios/obtener-habitaciones' #se reemplazara por el endpoint correspondiente cuando esté disponible
-                    #no logro que funcione con url_for('gestion_precios.obtener_habitaciones', _external=True)
-    try:                       
-        response = requests.get(obtener_habitaciones)
-        if response.status_code == 200:
-            datos = response.json()
-            return render_template('gestion_precios.html', datos=datos)
-        elif response.status_code == 404:
-            return render_template('404.html')
-        else:
-            return render_template('404.html')  #deberia ser pagina de error 500
-    except requests.RequestException:
-        return render_template('404.html')      #deberia ser pagina de error 500
-    
-    
-@app.route('/actualizar_precios', methods=['POST'])
-def modificar_precios_habitaciones():
-    id_habitacion = request.form['id']
-    nuevo_precio = str(request.form['precio_noche'])
-    actualizar_precio = 'http://127.0.0.1:5000/gestion_precios/gestion_precios'
-                     #no logro que funcione con url_for('gestion_precios.actualizar_precios_habitaciones', _external=True)
-    try:
-        response = requests.patch(actualizar_precio, json={'id': id_habitacion, 'precio_noche': nuevo_precio})
-
-        if response.status_code == 200:
-            return redirect(url_for('pedir_precios_habitaciones', _external=True)) 
-        elif response.status_code == 404:
-            return render_template('404.html')
-        else:
-            return render_template('404.html') #deberia ser pagina de error 500
-    except requests.RequestException:
-        return render_template('404.html')     #deberia ser pagina de error 500
-
-@app.route('/actualizar_precios', methods = ['GET'])
-def pedir_precios_habitaciones():
-    obtener_habitaciones = 'http://127.0.0.1:5000/gestion_precios/obtener-habitaciones' #se reemplazara por el endpoint correspondiente cuando esté disponible
-                    #no logro que funcione con url_for('gestion_precios.obtener_habitaciones', _external=True)
-    try:                       
-        response = requests.get(obtener_habitaciones)
-        if response.status_code == 200:
-            datos = response.json()
-            return render_template('gestion_precios.html', datos=datos)
-        elif response.status_code == 404:
-            return render_template('404.html')
-        else:
-            return render_template('404.html')  #deberia ser pagina de error 500
-    except requests.RequestException:
-        return render_template('404.html')      #deberia ser pagina de error 500
-    
-    
-@app.route('/actualizar_precios', methods=['POST'])
-def modificar_precios_habitaciones():
-    id_habitacion = request.form['id']
-    nuevo_precio = str(request.form['precio_noche'])
-    actualizar_precio = 'http://127.0.0.1:5000/gestion_precios/gestion_precios'
-                     #no logro que funcione con url_for('gestion_precios.actualizar_precios_habitaciones', _external=True)
-    try:
-        response = requests.patch(actualizar_precio, json={'id': id_habitacion, 'precio_noche': nuevo_precio})
-
-        if response.status_code == 200:
-            return redirect(url_for('pedir_precios_habitaciones', _external=True)) 
-        elif response.status_code == 404:
-            return render_template('404.html')
-        else:
-            return render_template('404.html') #deberia ser pagina de error 500
-    except requests.RequestException:
-        return render_template('404.html')     #deberia ser pagina de error 500
-
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5001, debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
