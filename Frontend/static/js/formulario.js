ec93b453470e3439c66591dd3f5d33b90bd28d26
CarlaMendoza90 <mendozacoronadocarla@gmail.com>
2024-06-26 04:00:27 -0300

    refactor: Mis mensajes eran de color blanco no se porque pero ahora los volvi a poner de rojo. Ahora aparce un mensaje flash avisando que se envio el mensaje. Antes, luego de enviar el formulario, si recargabas la pagina se enviaba otra vez pero ya lo arregle.

--- Frontend/static/js/formulario.js
+++ Frontend/static/js/formulario.js
@@ -61,3 +61,9 @@ function isValidEmail(email){
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
+
+$(document).ready(function() {
+    setTimeout(function() {
+        $('.alert').fadeOut('slow');
+    }, 4500);
+});
